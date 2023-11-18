6b3d5b13a7055afe08f1c772e2a28ec9a414c104
lcsh0s <decastro@et.esiea.fr>
2023-11-18 17:50:29 +0100

    images updated

--- lab6/data/draw.py
+++ lab6/data/draw.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load data from CSV
-csv_files = ['data_20000000.csv', 'data_10000000.csv', 'data_1000000.csv', 'data_100000.csv'   ]
 
-for f in csv_files:
-    df = pd.read_csv(f)
+
+def draw(s):
+    df = pd.read_csv(s)
     plt.plot(df['NumThreads'], df['ExecutionTime'], marker='o')
     plt.title('Execution Time vs NumThreads')
     plt.xlabel('NumThreads')
     plt.ylabel('Execution Time')
     plt.grid(True)
     plt.savefig(f'drawing_{f.split(".")[0].split("_")[1]}.png', dpi=300)
+    
+csv_files = ['data_20000000.csv', 'data_10000000.csv', 'data_1000000.csv', 'data_100000.csv'   ]
+
+csv_files.reverse()
+for f in csv_files:
+    draw(f)
